2ffa7fb4679e16a9ad36c5c57131a6c1e61e865c
TValentinPerezG <tperezg@fi.uba.ar>
2026-05-04 20:09:50 +0000

    añadimos solucion al ejercicio de tarea de la clase del 29 de abril

--- clases/clase.29.4.26/ejercicios/ejer_7_29.4.26.c
+++ clases/clase.29.4.26/ejercicios/ejer_7_29.4.26.c
@@ -5,3 +5,56 @@
 
 #include <stdio.h>
 
+#define MAX 200
+
+typedef int tvec[MAX];
+
+void ingreso_valores(tvec vec, int *cont){
+    int ingreso;
+    *cont = 0;
+
+    do{
+        printf("Ingrese valores. Si quiere dejar de ingresar ponga -1: ");
+        scanf("%i", &ingreso);
+
+        if(ingreso >= 0){
+            vec[*cont] = ingreso;
+            *cont = *cont + 1;
+        }
+        else if(ingreso != -1){
+            printf("\nEl valor ingresado no es valido\n");
+        }
+    }while(ingreso != -1);
+}
+
+void invertir_vector(tvec vec, int cont){
+    int aux;
+    int i;
+
+    for(i=0; i < cont/2 ;i++){
+        aux = vec[i];
+        vec[i] = vec[cont - 1 - i];
+        vec[cont -1 - i] = aux;
+    }
+}
+
+void mostrar_vector(tvec vec, int cont){
+    int i;
+
+    for(i=0; i < cont ; i++){
+        printf("El numero %i esta en la posicion %i\n", vec[i], i);
+    }
+}
+
+int main(){
+    tvec vector;
+    int contador = 0;
+
+    ingreso_valores(vector, &contador);
+
+    invertir_vector(vector, contador);
+
+    mostrar_vector(vector, contador);
+
+    return 0;
+}
